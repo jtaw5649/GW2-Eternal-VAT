6e54acd23edfc8497514c2f89414c53ef62b25cf
jtaw <28190715-jtaw@users.noreply.gitlab.com>
2025-07-15 19:51:33 +0000

    Fix ESLint v9 config and CI/CD webhook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,48 @@
-// .eslintrc.js
-module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-        jest: true
+module.exports = [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                console: "readonly",
+                process: "readonly",
+                Buffer: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly",
+                exports: "writable",
+                module: "writable",
+                require: "readonly",
+                global: "readonly",
+                setTimeout: "readonly",
+                setInterval: "readonly",
+                clearTimeout: "readonly",
+                clearInterval: "readonly",
+                setImmediate: "readonly",
+                clearImmediate: "readonly"
+            }
+        },
+        rules: {
+            "indent": ["error", 4],
+            "linebreak-style": ["error", "unix"],
+            "quotes": ["error", "single"],
+            "semi": ["error", "always"],
+            "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+        }
     },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    rules: {
-        'indent': ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+    {
+        files: ["tests/**/*.js"],
+        languageOptions: {
+            globals: {
+                jest: "readonly",
+                describe: "readonly",
+                test: "readonly",
+                expect: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly"
+            }
+        }
     }
-};
+];
