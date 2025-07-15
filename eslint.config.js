0ff3fd8d06ead6101670a9f2c35d9d0aff4de736
jtaw <28190715-jtaw@users.noreply.gitlab.com>
2025-07-15 21:15:10 +0100

    fix

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                process: 'readonly',
+                console: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                exports: 'readonly',
+                __dirname: 'readonly',
+                Buffer: 'readonly',
+                setTimeout: 'readonly',
+                setInterval: 'readonly',
+                clearTimeout: 'readonly',
+                clearInterval: 'readonly',
+                global: 'readonly',
+                jest: 'readonly',
+                describe: 'readonly',
+                test: 'readonly',
+                expect: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+                beforeAll: 'readonly',
+                afterAll: 'readonly'
+            }
+        },
+        rules: {
+            'indent': ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+        }
+    },
+    {
+        ignores: ['node_modules/**', 'coverage/**', 'dist/**', 'build/**']
+    }
+];
